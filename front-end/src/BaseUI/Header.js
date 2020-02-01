ecee6859db0bbd57c69f6e69a3049871bb824eab
naveed <nksherani@outlook.com>
2020-02-01 18:20:22 +0500

    little ui changes in labels

--- front-end/src/BaseUI/Header.js
+++ front-end/src/BaseUI/Header.js
@@ -12,7 +12,7 @@ class Header extends React.Component
         return (
 
             <div className="jumbotron text-center">
-                <h1>School Management System</h1>
+                <h1>E-Commerce Portal</h1>
                 <p>Resize this responsive page to see the effect!</p> 
             </div>
         )
--- front-end/src/BaseUI/NavigationBar.js
+++ front-end/src/BaseUI/NavigationBar.js
@@ -11,15 +11,15 @@ class NavigationBar extends React.Component
     render=()=>{
         return (
                 <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">School Management Portal</Navbar.Brand>
+                <Navbar.Brand href="/">E-Commerce Portal</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#link">Link</Nav.Link>
                     <NavDropdown title="Product Management" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">View Products</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Add Product</NavDropdown.Item>
+                        <NavDropdown.Item href="/Products">View Products</NavDropdown.Item>
+                        <NavDropdown.Item href="/Products/Add">Add Product</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
